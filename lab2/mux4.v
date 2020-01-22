39e8bb6c7f0b3bf5fcc0293e2f0ccc9fb0790e1e
Haocheng Hu <hc.hu@utoronto.ca>
2020-01-22 03:36:18 -0500

    wave4 done

--- lab2/mux4.v
+++ lab2/mux4.v
@@ -1,18 +1,34 @@
-//SW[2:0] data inputs
-//SW[9] select signal
+//SW[4:0] data inputs
+//SW[9] (s0) and SW[8] (s1) select signal
 
 //LEDR[0] output display
 
 module mux4(LEDR, SW);
     input [9:0] SW;
     output [9:0] LEDR;
+    wire up, down;
 
     mux2to1 u0(
         .x(SW[0]),
         .y(SW[1]),
         .s(SW[9]),
+        .m(up)
+        );
+
+    mux2to1 u1(
+        .x(SW[2]),
+        .y(SW[3]),
+        .s(SW[9]),
+        .m(down)
+        );
+
+    mux2to1 u2(
+        .x(up),
+        .y(down),
+        .s(SW[8]),
         .m(LEDR[0])
         );
+
 endmodule
 
 module mux2to1(x, y, s, m);
