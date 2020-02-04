a64e69147f82bcd7dddfab3901451e89473dfa12
Haocheng Hu <hc.hu@utoronto.ca>
2020-02-03 23:15:28 -0500

    lab4 p2 add proper case statement and additional outputs

--- lab4/part2/alulab4.v
+++ lab4/part2/alulab4.v
@@ -24,13 +24,15 @@ module alu(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 
     always @(*)
 	begin
-		case(KEY[2:0])
+		case(SW[7:5])
 			0: ALUout = {3'b000, f0};
 			1: ALUout = {3'b000, f1};
 			2: ALUout = SW[7:4] + SW[3:0];
 			3: ALUout = {SW[7:4] | SW[3:0], SW[7:4] ^ SW[3:0]};
 			4: ALUout = {7'b0000000, SW[7] | SW[6] | SW[5] | SW[4] | SW[3] | SW[2] | SW[1] | SW[0]};
 			5: ALUout = SW[7:0];
+            6: ALUout = 8'b00000000;
+            7: ALUout = 8'b00000000;
 			default: ALUout = 8'b00000000;
 		endcase
 	end
